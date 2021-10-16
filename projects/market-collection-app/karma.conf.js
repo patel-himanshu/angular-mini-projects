0f86f355dafde16c241cc12a9f75177a61bcd45e
Himanshu Patel <44549418+patel-himanshu@users.noreply.github.com>
2021-10-17 04:31:40 +0530

    chore: Project folder restructure

--- projects/market-collection-app/karma.conf.js
+++ projects/market-collection-app/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/market-collection-app'),
+      dir: require('path').join(__dirname, '../../coverage/market-collection-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
